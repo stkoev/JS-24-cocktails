8c180383fdc08dee817dc711984d49938c1dd951
Stefan Koev <st.koev@outlook.com>
2022-10-08 17:19:56 +0300

    setDrink in local storage

--- src/presentDrinks.js
+++ src/presentDrinks.js
@@ -1,16 +1,16 @@
 import fetchDrinks from "./fetchDrinks.js";
 import displayDrinks from "./displayDrinks.js";
-// import toggleLoading from "./toggleLoading.js";
-import { hideLoading, showLoading } from "./toggleLoading.js";
+import setDrink from "./setDrink.js";
 
 const showDrinks = async (url) => {
   // fetch drinks
-  showLoading();
   const data = await fetchDrinks(url);
 
   // display drniks
-  hideLoading();
   const section = await displayDrinks(data);
+  if (section) {
+    setDrink(section);
+  }
 };
 
 export default showDrinks;
---
+++ src/setDrink.js
@@ -0,0 +1,9 @@
+const setDrink = function (section) {
+  section.addEventListener("click", (e) => {
+    // e.preventDefault();
+    const id = e.target.parentElement.dataset.id;
+    localStorage.setItem("drink", id);
+  });
+};
+
+export default setDrink;
